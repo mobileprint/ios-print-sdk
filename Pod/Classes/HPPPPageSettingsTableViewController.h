bc43844867b634f9e048dad257a767264a3bf955
arctouch-andregatti <andre.gatti@arctouch.com>
2014-12-09 11:39:16 -0800

    [print-flow] Add documentation.

--- Pod/Classes/HPPPPageSettingsTableViewController.h
+++ Pod/Classes/HPPPPageSettingsTableViewController.h
@@ -16,8 +16,18 @@
 
 @interface HPPPPageSettingsTableViewController : UITableViewController
 
+/*!
+ If this value is true, the black & white filter option is shown together with the paper size and paper type in the page settings screen.
+ 
+ By default, this value is false and the black & white filter option is shown.
+ */
 @property (assign, nonatomic) BOOL hideBlackAndWhiteOption;
+
+/*!
+ The image to show in the preview view.
+ */
 @property (strong, nonatomic) UIImage *image;
+
 @property (nonatomic, weak) id<PGPageSettingsTableViewControllerDelegate> delegate;
 @property (nonatomic, weak) HPPPPageViewController *pageViewController;
 
@@ -26,6 +36,11 @@
 
 @protocol PGPageSettingsTableViewControllerDelegate <NSObject>
 
+/*!
+ Indicates when the print flows finishes after the print job is sent to the print pool.
+ \param pageSettingsTableViewController The class that calls the delegate
+ \returns N/A.
+ */
 - (void)pageSettingsTableViewControllerDidFinishPrintFlow:(HPPPPageSettingsTableViewController *)pageSettingsTableViewController;
 - (void)pageSettingsTableViewControllerDidCancelPrintFlow:(HPPPPageSettingsTableViewController *)pageSettingsTableViewController;
 
