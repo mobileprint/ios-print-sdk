cfa3619e9b4878f2f51d07a62b2c02d8c8295870
James <trask@hp.com>
2014-12-11 13:27:27 -0800

    [documentation] updated HPPPPageSettingsTableViewController.h

--- Pod/Classes/Public/HPPPPageSettingsTableViewController.h
+++ Pod/Classes/Public/HPPPPageSettingsTableViewController.h
@@ -15,34 +15,79 @@
 @protocol PGPageSettingsTableViewControllerDelegate;
 @protocol PGPageSettingsTableViewControllerDataSource;
 
+/*!
+ * @abstract The view controller class for displaying the print preview
+ * @description This class implements the view controller used for displaying the print preview screen and associated page settings used for printing.
+ */
 @interface HPPPPageSettingsTableViewController : UITableViewController
 
 /*!
- The image to show in the preview view.
+ * @abstract The image used for printing
+ * @description This image is used for both displaying in the print preview and in the actual print job itself.
  */
 @property (strong, nonatomic) UIImage *image;
 
+/*!
+ * @abstract Notified when print events occur
+ * @description This delegate is notified whenever the print flow completes or gets canceled.
+ * @seealso PGPageSettingsTableViewControllerDelegate
+ */
 @property (nonatomic, weak) id<PGPageSettingsTableViewControllerDelegate> delegate;
+
+/*!
+ * @abstract Provides the printable image asset
+ * @description The data source is used to provide an image to use for printing. A new image is requested whenever relevant parameters change (e.g. the user picks a new page size).
+ * @seealso PGPageSettingsTableViewControllerDataSource
+ */
 @property (nonatomic, weak) id<PGPageSettingsTableViewControllerDataSource> dataSource;
+
+/*!
+ * @abstract The graphical page representation part of the print preview
+ * @description The pageViewController is reponsible for displaying a graphical representation of the print on the page. It is one part of the overall page settings view also known as the print preview.
+ */
 @property (nonatomic, weak) HPPPPageViewController *pageViewController;
 
 @end
 
-
+/*!
+ * @abstract Defines a delegate protocal for reporting print events
+ * @seealso PGPageSettingsTableViewControllerDataSource
+ */
 @protocol PGPageSettingsTableViewControllerDelegate <NSObject>
 
 /*!
- Indicates when the print flows finishes after the print job is sent to the print pool.
- \param pageSettingsTableViewController The class that calls the delegate
- \returns N/A.
+ * @abstract Called when the print flow finishes successfully
+ * @description This delegate method is called when the print flow finishes successfully. This means that the print job was sent to the printer without error. It does not mean that the job was completed and printed without error, just that the job was queued successfully. Errors such as out-of-paper could still occur after this method is called.
+ * @param pageSettingsTableViewController The page settings view controller calling the method
+ * @returns Nothing
+ * @seealso pageSettingsTableViewControllerDidCancelPrintFlow
  */
 - (void)pageSettingsTableViewControllerDidFinishPrintFlow:(HPPPPageSettingsTableViewController *)pageSettingsTableViewController;
+
+/*!
+ * @abstract Called when the print flow is canceled
+ * @description This method is called when the print flow is canceled by the user. If the print job is queued successfully but subsequently canceled by the user in the Print Center, this method is not called.
+ * @param pageSettingsTableViewController The page settings view controller calling the method
+ * @returns Nothing
+ * @seealso pageSettingsTableViewControllerDidFinishPrintFlow
+ */
 - (void)pageSettingsTableViewControllerDidCancelPrintFlow:(HPPPPageSettingsTableViewController *)pageSettingsTableViewController;
 
 @end
 
+/*!
+ * @abstract Defines a data source protocal for requesting the printable image
+ * @seealso PGPageSettingsTableViewControllerDelegate
+ */
 @protocol PGPageSettingsTableViewControllerDataSource <NSObject>
 
+/*!
+ * @abstract Called when a new printable image is needed
+ * @description This method is called when initiating the print flow or whenever relevant parameters are changed (e.g. page size).
+ * @param paper The HPPPPaper object that the image will be laid out on
+ * @returns UIImage object for the printable image
+ * @seealso HPPPaper
+ */
 - (UIImage *)pageSettingsTableViewControllerRequestImageForPaper:(HPPPPaper *)paper;
 
 @end
