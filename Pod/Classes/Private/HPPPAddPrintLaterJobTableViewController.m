038f1c4437fb92977f62a5595a6eb0f1ef6695c6
Chrissy, Eric, and James <diegopair1@hp.com>
2015-07-06 14:38:17 -0700

    Fixing keyboard and page range display on iPhone 4

--- Pod/Classes/Private/HPPPAddPrintLaterJobTableViewController.m
+++ Pod/Classes/Private/HPPPAddPrintLaterJobTableViewController.m
@@ -137,6 +137,7 @@ NSString * const kPageRangeNoPages = @"No pages selected";
     
     self.tableView.tableFooterView = self.footerView;
     self.activeCellColor = self.addToPrintQLabel.textColor;
+    self.numCopiesStepper.tintColor = self.addToPrintQLabel.textColor;
     
     [self reloadJobSummary];
     [self configureMultiPageView];
@@ -147,11 +148,11 @@ NSString * const kPageRangeNoPages = @"No pages selected";
     [super viewWillAppear:animated];
     self.navigationBarTintColor = self.navigationController.navigationBar.barTintColor;
     [[NSNotificationCenter defaultCenter] postNotificationName:kHPPPTrackableScreenNotification object:nil userInfo:[NSDictionary dictionaryWithObject:kAddJobScreenName forKey:kHPPPTrackableScreenNameKey]];
+}
 
-    CGRect desiredSmokeyViewFrame = self.view.frame;
-    desiredSmokeyViewFrame.size.height += desiredSmokeyViewFrame.origin.y;
-    desiredSmokeyViewFrame.origin.y = 0;
-    
+- (void)setSmokeyViewFrame
+{
+    CGRect desiredSmokeyViewFrame = [self.view convertRect:self.view.frame fromView:[self.view superview]];
     self.smokeyView.frame = desiredSmokeyViewFrame;
 }
 
@@ -244,11 +245,11 @@ NSString * const kPageRangeNoPages = @"No pages selected";
         }
     } else {
         
-        CGRect desiredFrame = self.tableView.frame;
-        desiredFrame.origin.y = 0;
+        [self setSmokeyViewFrame];
+        CGRect desiredFrame = self.smokeyView.frame;
         
         CGRect startingFrame = desiredFrame;
-        startingFrame.origin.y = self.view.frame.origin.y + self.view.frame.size.height;
+        startingFrame.origin.y = self.smokeyView.frame.origin.y + self.smokeyView.frame.size.height;
         
         if(cell == self.pageRangeCell) {
             self.pageRangeView.frame = startingFrame;
