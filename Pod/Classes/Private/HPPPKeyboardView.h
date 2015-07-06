5e01985d85914509051af9e7f4d057da8e53caf1
Christine Harris <chrissy.murray@hp.com>
2015-07-05 21:26:58 -0700

    Documentation changes

--- Pod/Classes/Private/HPPPKeyboardView.h
+++ Pod/Classes/Private/HPPPKeyboardView.h
@@ -15,13 +15,29 @@
 
 @protocol HPPPKeyboardViewDelegate;
 
+/*!
+ * @abstract A class for displaying a single textfield and keyboard
+ */
 @interface HPPPKeyboardView : HPPPOverlayEditView
 
+/*!
+ * @abstract A delegate that is called when the user is finished entering text into the text field
+ * @seealso HPPPKeyboardViewDelegate
+ */
 @property (strong, nonatomic) id<HPPPKeyboardViewDelegate> delegate;
 
 @end
 
+/*!
+ * @abstract Protocol used to indicate that the user has completed entering text in the text field
+ */
 @protocol HPPPKeyboardViewDelegate <NSObject>
 @optional
+
+/*!
+ * @abstract Called when the user has completed entering text in the text field
+ * @param view The keyboard view
+ * @param text The text entered by the user
+ */
 - (void)didFinishEnteringText:(HPPPKeyboardView *)view text:(NSString *)text;
 @end
