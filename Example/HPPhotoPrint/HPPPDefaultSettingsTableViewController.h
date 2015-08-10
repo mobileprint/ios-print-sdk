0c4831e76c37bbba341f1f9be29ec1671c54c105
Chrissy, Eric, and James <diegopair1@hp.com>
2015-08-10 12:11:46 -0700

    Scratch work for Example UI configuration

---
+++ Example/HPPhotoPrint/HPPPDefaultSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HPPPDefaultSettingsTableViewController.h
+//  HPPhotoPrint
+//
+//  Created by Bozo on 8/10/15.
+//  Copyright (c) 2015 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPPPDefaultSettingsTableViewController : UITableViewController
+
+@end
