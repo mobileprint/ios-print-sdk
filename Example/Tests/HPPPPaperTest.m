691c6b34ceae7787d74ab21775d69b26c132ea71
James <trask@hp.com>
2015-09-25 13:52:53 -0700

    [tests] add OCMock pod for stubbing and mocking in tests

---
+++ Example/Tests/HPPPPaperTest.m
@@ -0,0 +1,55 @@
+//
+// Hewlett-Packard Company
+// All rights reserved.
+//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import <HPPP.h>
+#import <HPPPPaper.h>
+#import <OCMock/OCMock.h>
+
+@interface HPPPPaperTest : XCTestCase
+
+@end
+
+@implementation HPPPPaperTest
+{
+}
+
+#pragma mark - Setup tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Custom setup here
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+    // Custom teardown here
+}
+
+#pragma mark - Test 'init'
+
+- (void)testprinterPaperSize
+{
+}
+
+- (void)testprinterPaperSizeOverrideWidth
+{
+}
+
+- (void)testprinterPaperSizeOverrideHeight
+{
+}
+
+@end
