a5f7ff9b0bb7eaec9d5a869bebe7d0f32c47affe
Eric <eric.talle@hp.com>
2015-10-12 15:59:14 -0700

    small mod to jenkins_build.sh

--- Example/jenkins_build.sh
+++ Example/jenkins_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
-xcodebuild -workspace ../HPPhotoPrint.xcworkspace/ -scheme HPPhototPrint-cal -configuration Debug -sdk iphonesimulator  ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR="/Users/Shared/Jenkins/Home/jobs/hppp-example-app-ios/workspace/build" clean build
+xcodebuild -workspace HPPhotoPrint.xcworkspace/ -scheme HPPhototPrint-cal -configuration Debug -sdk iphonesimulator  ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR="/Users/Shared/Jenkins/Home/jobs/hppp-example-app-ios/workspace/build" clean build
