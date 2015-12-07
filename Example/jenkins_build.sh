c7b83dd14bdc375d56c0996894eb81064d137ddb
James <trask@hp.com>
2015-12-07 15:17:57 -0800

    [US4236] add 'xcpretty' to clean up calabash build output for jenkins

--- Example/jenkins_build.sh
+++ Example/jenkins_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
-xcodebuild -workspace MobilePrintSDK.xcworkspace/ -scheme MobilePrintSDK-cal -configuration Debug -sdk iphonesimulator  ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR="/Users/Shared/Jenkins/Home/jobs/ios-print-sdk-sample-app/workspace/build" clean build
+xcodebuild -workspace MobilePrintSDK.xcworkspace/ -scheme MobilePrintSDK-cal -configuration Debug -sdk iphonesimulator  ONLY_ACTIVE_ARCH=NO CONFIGURATION_BUILD_DIR="/Users/Shared/Jenkins/Home/jobs/ios-print-sdk-sample-app/workspace/build" clean build | xcpretty && exit ${PIPESTATUS[0]}
