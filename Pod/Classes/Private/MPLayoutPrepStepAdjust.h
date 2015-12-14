3b9f7e29e93e4fee2cb94787b17c2dd87b08c822
Chrissy Harris <christine.harris10@gmail.com>
2015-12-14 15:37:42 -0800

    [DE3545] update headers; add unit tests for fit layout

--- Pod/Classes/Private/MPLayoutPrepStepAdjust.h
+++ Pod/Classes/Private/MPLayoutPrepStepAdjust.h
@@ -1,9 +1,13 @@
 //
-//  MPLayoutPrepStepAdjust.h
-//  Pods
+// HP Inc.
+// All rights reserved.
 //
-//  Created by James Trask on 12/9/15.
-//
+// This file, its contents, concepts, methods, behavior, and operation
+// (collectively the "Software") are protected by trade secret, patent,
+// and copyright laws. The use of the Software is governed by a license
+// agreement. Disclosure of the Software to third parties, in any form,
+// in whole or in part, is expressly prohibited except as authorized by
+// the license agreement.
 //
 
 #import "MPLayoutPrepStep.h"
