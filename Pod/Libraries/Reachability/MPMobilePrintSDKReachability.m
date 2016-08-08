a90b0fec86055c837e6a21fe2cdd45e2502b1585
James <trask@hp.com>
2016-08-08 10:00:59 -0700

    [US4089] fix potentially conflicting symbol

--- Pod/Libraries/Reachability/MPMobilePrintSDKReachability.m
+++ Pod/Libraries/Reachability/MPMobilePrintSDKReachability.m
@@ -55,7 +55,7 @@
 #import "MPMobilePrintSDKReachability.h"
 
 
-NSString *kMPReachabilityChangedNotification = @"kMPNetworkReachabilityChangedNotification";
+NSString *kMPMobilePrintSDKReachabilityChangedNotification = @"kMPMobilePrintSDKReachabilityChangedNotification";
 
 
 #pragma mark - Supporting functions
@@ -91,7 +91,7 @@ static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
 
     MPMobilePrintSDKReachability* noteObject = (__bridge MPMobilePrintSDKReachability *)info;
     // Post a notification to notify the client that the network reachability changed.
-    [[NSNotificationCenter defaultCenter] postNotificationName: kMPReachabilityChangedNotification object: noteObject];
+    [[NSNotificationCenter defaultCenter] postNotificationName: kMPMobilePrintSDKReachabilityChangedNotification object: noteObject];
 }
 
 
